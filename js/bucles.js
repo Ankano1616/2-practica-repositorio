d19a61fe6ea73bcb73f71f8260d9cf39895f9d25
Andrea Mierez <Andrea M.M>
2024-05-26 18:45:27 -0300

    guardo el proyecto

---
+++ js/bucles.js
@@ -0,0 +1,44 @@
+//estructuras repetitivas INDERTERMINADAS (while,do while...)
+//porque DEPENDE DE UNA CONDICION
+//let limite=5;
+//let contador=0;
+//let lista=document.querySelector('#listaWhile');
+//console.log(lista);
+
+//agrego esto para que lo muestre el git status
+//grupos sanguineos: 0- A- B - AB // Factor Rh+ o Rh -
+//let grupoSanguineo="A";
+//switch - case
+//if anidados
+//switch (grupoSanguineo){
+    //case "0+":
+//    case "0":
+  //
+
+//console.log('Fin de algoritmo');
+
+
+//Clase 13
+//TIPOS DE DATOS
+//datos primitivos
+//booleanm numeros, bigint, string,simbol, y undefines
+
+
+//complejos
+//Array - areglso (vestor) -->listas en pythob
+//Objetos, atributos, metodos -->python diccionario
+//null
+
+//Java Script
+//ojo DISTINGUE entre las minusculasy mayusculas
+typeof 5 // me duvuelve 'number'
+typeof 3.1416 // me devuelve 'number
+typeof 5n // lo vuelvee 'bigInt'
+typeof 'Java Script'// devuelve que es STRING, iual usar '' o ""
+typeof true// devuelve boolean
+typeof false // devuelve boolean
+typeof True// indeined
+
+//debilmente tipado, hace cosas raras.. fijarse si opera o concatena
+
+console.log('HOLA MUNDO'); 
